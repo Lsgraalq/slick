fbba902a178a766d161637578e89de8f8f4d888b
Lsgraalq <90512728+Lsgraalq@users.noreply.github.com>
2022-02-08 18:33:15 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.sl').slick({
+    autoplay: true,
+    prevArrow: '<button type="button" class="prev"></button>',
+    nextArrow: '<button type="button" class="prev"></button>',
+    appendArrows: $('.buttons'),
+    slidesToShow: 2
+  });
+});
